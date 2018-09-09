57904798de17f3bc3abe634cd06e4605dd459420
AFresnedo <AFresnedo@users.noreply.github.com>
2018-09-09 13:58:03 -0700

    add comment block about mutations

--- src/index.js
+++ src/index.js
@@ -111,3 +111,34 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+/* AVOID MUTATIONS (first example: slice array of marks, instead of editing):
+ *
+ * NOTE Complex Features Become Simple
+ *
+ * Immutability makes complex features much easier to implement. Later in this
+ * tutorial, we will implement a “time travel” feature that allows us to review
+ * the tic-tac-toe game’s history and “jump back” to previous moves. This
+ * functionality isn’t specific to games — an ability to undo and redo certain
+ * actions is a common requirement in applications. Avoiding direct data
+ * mutation lets us keep previous versions of the game’s history intact, and
+ * reuse them later.
+ *
+ * NOTE Detecting Changes
+ *
+ * Detecting changes in mutable objects is difficult because they are modified
+ * directly. This detection requires the mutable object to be compared to
+ * previous copies of itself and the entire object tree to be traversed.
+ *
+ * Detecting changes in immutable objects is considerably easier. If the
+ * immutable object that is being referenced is different than the previous
+ * one, then the object has changed.
+ *
+ * NOTE Determining When to Re-render in React
+ *
+ * The main benefit of immutability is that it helps you build pure components
+ * in React. Immutable data can easily determine if changes have been made
+ * which helps to determine when a component requires re-rendering.
+ *
+ * You can learn more about shouldComponentUpdate() and how you can build pure
+ * components by reading Optimizing Performance. */
